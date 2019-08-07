11f39f6041acaf18af61b7bf2775681f8381fd5d
Flatiron School <email.the.anderson@gmail.com>
2019-08-07 19:59:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    para.textContent = "This is really cool!"
+  });
+
+function fetchImage() {
+   return fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(resp => resp.json())
+  .then(json => renderImages(json));
+
+}
+
+function renderImages(json) {
+    const dogIMG = document.querySelector('dog-images-container')
+    json.forEach(message => {
+        const img = document.createElement('img')
+        img.src = `<img src = ${message}>`
+        dogIMG.appendChild(img)
+    })
+
+}
